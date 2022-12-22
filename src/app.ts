5c0a357812a0710fed405a96d4b789d0e66d9d22
Kleverton Santana Montalvão <tonprofissional9@gmail.com>
2022-12-21 22:34:27 -0300

    Mongoose references removed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import Routes from "./routes/routes";
-import mongoose from "mongoose";
 
 dotenv.config();
-mongoose.connect("mongodb://127.0.0.1/my_database");
 
 const app: express.Application = express();
 const port: string = process.env.LOCAL_PORT || "8080";
